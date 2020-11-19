744c76294fb42e9c7d4ea60f8fb03b77cf654cfd
Klimek Jakub <jklimek@MacBook-Pro-Klimek.local>
2020-11-19 11:08:25 +0100

    port fix

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 
-app.listen(PORT,function(){
-    console.log(`Serwer dziala na porcie ${PORT}`)
-})
+app.listen(process.env.PORT || 3000)
 
 app.use(express.static('static'))
 
